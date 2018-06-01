921b24999eb0ad2098bc3bc8ce72af87e85dd064
FirstBuiltPC <lwenzel90@gmail.com>
2018-06-01 14:24:31 -0400

    Chapter 1 solutions to Headfirst java

---
+++ Ch1/BeerSong.java
@@ -0,0 +1,23 @@
+public class BeerSong{
+	public static void main (String[] args){
+		int beerNum = 99;
+		String word = "Bottles";
+
+		while (beerNum > 0){
+			if(beerNum ==1){
+				word = "bottle";
+			}
+
+			System.out.println(beerNum + " " + word + " of beer on the wall");
+			System.out.println(beerNum + " " + word + " of beer.");
+			System.out.println("Take one down\nPass it around");
+			beerNum--;
+
+			if(beerNum > 0){
+				System.out.println(beerNum + " " + word + " of beer on the wall!\n");
+			} else{
+				System.out.println("No more bottles of beer on the wall!\n");
+			}
+		}
+	}
+}
